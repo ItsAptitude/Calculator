a2e753091fe66e27f13dd1a7e2484b4c1d5aba72
Aptitude <79222443+ItsAptitude@users.noreply.github.com>
2021-10-27 20:17:38 +0200

    added history

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 public class Program
 {
+	public static List<string> Historik = new List<string>();
+	public static string plus;
+	public static string minus;
+	public static string division;
+	public static string multiply;
+
 	public static void Main()
 	{
-		Console.Title = "Aptitude's Calculator";
-
-		Console.WriteLine("Type Number 1");
+		Console.WriteLine("Skriv Nummer 1");
 		string tal1 = Console.ReadLine(); // läs tal 1
 
-		Console.WriteLine("Type Operator (+, -, *, /)");
+		Console.WriteLine("Skriv Operator (+, -, *, /)");
 		string op = Console.ReadLine(); // läs operator
 
-		Console.WriteLine("Type Number 2");
+		Console.WriteLine("Skriv Nummer 2");
 		string tal2 = Console.ReadLine(); // läs tal 2
 
 		float nytal1 = float.Parse(tal1); // gör tal 1 till float value eftersom vi vill kunna ha decimaler
@@ -19,31 +25,41 @@ public class Program
 
 		if (op == "+") // om operator är +
 		{
-			Console.WriteLine(tal1 + " + " + tal2 + " = " + (nytal1 + nytal2));
+			plus = tal1 + " + " + tal2 + " = " + (nytal1 + nytal2); // säger att stringen plus är nytal1 + nytal2
+			Historik.Add(plus); // lägger till hela uträkningen i historik listen
 		}
 		if (op == "-") // om operator är -
 		{
-			Console.WriteLine(tal1 + " - " + tal2 + " = " + (nytal1 - nytal2));
+			minus = tal1 + " - " + tal2 + " = " + (nytal1 - nytal2); // säger att stringen minus är nytal1 - nytal2
+			Historik.Add(minus); // lägger till hela uträkningen i historik listen
 		}
 
 		if (op == "/") // om operator är /
 		{
-			if (!(nytal2 == 0) && (!(nytal1 == 0))) // om tal 1 eller tal 2 är 0, så visar den fel meddelande
+			if (!(nytal2 == 0) && (!(nytal1 == 0))) // Kollar om nytal1 eller nytal 2 är 0
 			{
-				Console.WriteLine(tal1 + " / " + tal2 + " = " + (nytal1 / nytal2)); // om tal 1 eller tal 2 inte är 0
+				division = tal1 + " / " + tal2 + " = " + (nytal1 / nytal2); // säger att stringen division är nytal1 / nytal2
+				Historik.Add(division); // lägger till hela uträkningen i historik listen
 			}
 			else // om tal 1 eller 2 är 0
 			{
-				Console.WriteLine("You cannot divide with 0!\n");
+				Console.WriteLine("Du kan ej dividera med 0!\n");
 				Main();
 			}
 		}
 		if (op == "*") // om operator är *
 		{
-			Console.WriteLine(tal1 + " * " + tal2 + " = " + (nytal1 * nytal2));
+			multiply = tal1 + " * " + tal2 + " = " + (nytal1 * nytal2); // säger att stringen multiply är nytal1 * nytal2
+			Historik.Add(multiply); // lägger till hela uträkningen i historik listen
 		}
-
 		Console.WriteLine(""); // tom rad
-		Main(); // kör om Program.Main, vilket gör om hela koden från början
+
+		Console.WriteLine("Historik:");
+		foreach (string Item in Historik) // för varje string i listan Historik så skriver vi ut den stringen i consolen
+		{
+			Console.WriteLine(Item);
+		}
+		Console.WriteLine(""); // tom rad
+		Main(); // kör om Program.Main, vilket kör om hela koden från början
 	}
 }
